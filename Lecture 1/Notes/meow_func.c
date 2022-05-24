8c46941dfa3ee9bbea56929175a3a89c3a9362a5
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-24 10:05:10 +0200

    functions variables

--- Lecture 1/Notes/meow_func.c
+++ Lecture 1/Notes/meow_func.c
@@ -1,11 +1,41 @@
 #include <stdint.h>
 
-void meow(void) {
-    printf("meow\n");
+// to use it before declaration
+void baw(void);
+
+void meow(int x) {
+    for (int i = 0; i < x; i++) {
+        printf("Meow\n");
+    }
 }
+
+
+
 
 int main(void) {
     for (int i = 0; i < 50; i++) {
-        meow();
+        meow(3);
     }
+
+    int i = 0;
+    while (i < 30) {
+        baw();
+        i++;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// notice has been declared on the top of the file and used into main function
+void baw(void) {
+    printf("Baw\n");
 }
